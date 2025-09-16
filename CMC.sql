f01cc41af2a625926a9b5b60469797e7d57f4f35
BenjaminAlejandroZepeda <ben.zepeda@duocuc.cl>
2025-09-16 00:42:45 -0300

    Cambios 3

--- CMC.sql
+++ CMC.sql
@@ -30,7 +30,7 @@ CURSOR cur_auditorias(p_id_auditor NUMBER, p_mes_proceso NUMBER, p_anno_proceso 
 v_cant_auditorias NUMBER(8);
 v_monto_auditorias NUMBER(8);
 
--- variables TOTALIZADORAS
+-- variables ESCALARES
 
 v_comision_total_audit NUMBER(10,2);
 v_comision_monto_audit NUMBER(10,2);
@@ -38,6 +38,7 @@ v_comision_prof_critica NUMBER(10,2);
 v_comision_extra NUMBER(10,2);
 v_total_comision_audit NUMBER(10,2);
 v_total_comision_empresa NUMBER(10,2);
+
 
 
 
@@ -54,14 +55,11 @@ FOR reg_auditor IN cur_auditor LOOP
 
 
 -- Se inicializan las variables totalizadoras en cero  
--- variables TOTALIZADORAS
-
-    v_comision_cant_audit := 0;
-    v_comision_monto_audit := 0;
-    v_comision_prof_critica := 0;
-    v_comision_extra := 0;
-    v_total_comision_audit := 0;
-    v_total_comision_empresa := 0;
+v_res_total_auditores  := 0;
+v_res_total_con_auditorias    := 0;
+v_res_total_sin_auditorias    := 0;
+v_res_monto_total_auditorias  := 0;
+v_res_monto_total_comisiones := 0;
 
 -----SEGUNDO LOOP
 
@@ -106,20 +104,65 @@ v_comision_extra:= calcular_comision_extra(
 v_total_comision_audit := v_comision_cant_audit + v_comision_monto_audit + v_comision_prof_critica + v_comision_extra;
 
 
-
 -----TOTAL EMPRESA
-  
 
-    END LOOP;
+
 
  -- INSERTAR DATOS EN LA TABLA DE DETALLE
+INSERT INTO detalle_comisiones_auditorias_mes (
+    mes_proceso,
+    anno_proceso,
+    run_auditor,
+    nombre_auditor,
+    nombre_profesion,
+    comision_total_audit,
+    comision_monto_audit,
+    comision_prof_critica,
+    comision_extra,
+    total_comision_audit,
+    total_comision_empresa
+)
+VALUES (
+    SUBSTR(:b_fecha,-2),
+    SUBSTR(:b_fecha,1,4),
+    reg_auditor.numrun || '-' || reg_auditor.dvrun,
+    reg_auditor.nombre || ' ' || reg_auditor.appaterno || ' ' || reg_auditor.apmaterno,
+    (SELECT nombre_profesion FROM profesion WHERE cod_profesion = reg_auditor.cod_profesion),
+    v_comision_cant_audit,
+    v_comision_monto_audit,
+    v_comision_prof_critica,
+    v_comision_extra,
+    v_total_comision_audit,
+    v_total_comision_empresa
+);
 
 -----CALCULO DE LAS VARIABLES TOTALIZADORAS  
+
 
 END LOOP;
 
  -- INSERTAR DATOS EN LA TABLA DE RESUMEN
+    INSERT INTO resumen_comisiones_auditorias_mes (
+    mes_proceso,
+    anno_proceso,
+    nombre_profesion,
+    total_auditores,
+    total_con_auditorias,
+    total_sin_auditorias,
+    monto_total_auditorias,
+    monto_total_comisiones
+)
+VALUES (
+    SUBSTR(:b_fecha,-2),
+    SUBSTR(:b_fecha,1,4),
+    (SELECT nombre_profesion FROM profesion WHERE cod_profesion = reg_auditor.cod_profesion), 
+    v_res_total_auditores,
+    v_res_total_con_auditorias,
+    v_res_total_sin_auditorias,
+    v_res_monto_total_auditorias,
+    v_res_monto_total_comisiones
+);
 
+END LOOP;
 COMMIT;
-
 END;  
