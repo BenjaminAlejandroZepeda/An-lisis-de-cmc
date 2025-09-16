25e531cbaec4d1e822ef76392fa191baea815c1c
BenjaminAlejandroZepeda <ben.zepeda@duocuc.cl>
2025-09-16 12:21:52 -0300

    A

--- CMC.sql
+++ CMC.sql
@@ -1,168 +1,191 @@
-
 -- VARIABLES BIND
 VARIABLE b_fecha VARCHAR2(6);
 EXEC :b_fecha := '202301';
-
 
 VARIABLE p_valor_limite_com NUMBER;
 EXEC :p_valor_limite_com := 500000;
 
 DECLARE 
+    -- Variables para mes y año
+    v_mes_proceso  NUMBER(2);
+    v_anno_proceso NUMBER(4);
+    v_correlativo  NUMBER := 0;
 
-------CURSOR SIN PARAMETROS
+    -- Cursor para auditores con información completa
+    CURSOR cur_auditor IS
+    SELECT
+        a.id_auditor,
+        a.numrun,
+        a.nombre,
+        a.sueldo,
+        a.cod_profesion,
+        a.cod_tpcontrato,
+        p.nombre_profesion,
+        p.nivel_criticidad
+    FROM
+        auditor a
+        JOIN profesion p ON a.cod_profesion = p.cod_profesion;
 
-CURSOR cur_auditor IS
-    SELECT id_auditor, numrun, nombre, sueldo, cod_profesion, cod_tpcontrato
-    FROM auditor;
+    -- Cursor para empresas auditadas por un auditor
+    CURSOR cur_empresas (
+        p_id_auditor    NUMBER,
+        p_fecha_proceso VARCHAR2
+    ) IS
+    SELECT
+        cod_empresa,
+        SUM(monto_auditoria) AS monto_empresa
+    FROM
+        auditoria
+    WHERE
+        id_auditor = p_id_auditor
+        AND TO_CHAR(fin_auditoria, 'YYYYMM') = p_fecha_proceso
+    GROUP BY
+        cod_empresa;
 
-
-----CURSOR CON PARAMETROS  
-
-CURSOR cur_auditorias(p_id_auditor NUMBER, p_mes_proceso NUMBER, p_anno_proceso NUMBER) IS
-    SELECT 
-        cod_empresa, monto_auditoria, fin_auditoria
-    FROM auditoria
-    WHERE id_auditor = p_id_auditor
-    AND EXTRACT(MONTH FROM fin_auditoria) = p_mes_proceso
-    AND EXTRACT(YEAR FROM fin_auditoria) = p_anno_proceso;
-
------ESCALARES
-v_cant_auditorias NUMBER(8);
-v_monto_auditorias NUMBER(8);
-
--- variables ESCALARES
-
-v_comision_total_audit NUMBER(10,2);
-v_comision_monto_audit NUMBER(10,2);
-v_comision_prof_critica NUMBER(10,2);
-v_comision_extra NUMBER(10,2);
-v_total_comision_audit NUMBER(10,2);
-v_total_comision_empresa NUMBER(10,2);
-
-
-
-
+    -- Variables para comisiones
+    v_comision_cant_audit    NUMBER(12, 2);
+    v_comision_monto_audit   NUMBER(12, 2);
+    v_comision_prof_critica  NUMBER(12, 2);
+    v_comision_extra         NUMBER(12, 2);
+    v_total_comision_audit   NUMBER(12, 2);
+    v_total_comision_empresa NUMBER(12, 2);
+    v_total_auditor          NUMBER(14, 2);
 BEGIN
-  -- TRUNCA TABLAS
-  EXECUTE IMMEDIATE 'TRUNCATE TABLE DETALLE_COMISIONES_AUDITORIAS_MES';
-  EXECUTE IMMEDIATE 'TRUNCATE TABLE RESUMEN_COMISIONES_AUDITORIAS_MES';
-  EXECUTE IMMEDIATE 'TRUNCATE TABLE ERROR_PROCESO';
+    -- Extraer mes y año de :b_fecha
+    v_anno_proceso := TO_NUMBER(SUBSTR(:b_fecha, 1, 4));
+    v_mes_proceso  := TO_NUMBER(SUBSTR(:b_fecha, -2));
 
-------PRIMER LOOP
+    -- Truncar tablas
+    EXECUTE IMMEDIATE 'TRUNCATE TABLE DETALLE_COMISIONES_AUDITORIAS_MES';
+    EXECUTE IMMEDIATE 'TRUNCATE TABLE RESUMEN_COMISIONES_AUDITORIAS_MES';
+    EXECUTE IMMEDIATE 'TRUNCATE TABLE ERROR_PROCESO';
 
-FOR reg_auditor IN cur_auditor LOOP 
+    -- Bucle por cada auditor
+    FOR reg_auditor IN cur_auditor LOOP
+        -- Inicializar variables
+        v_comision_cant_audit    := 0;
+        v_comision_monto_audit   := 0;
+        v_comision_prof_critica  := 0;
+        v_comision_extra         := 0;
+        v_total_comision_audit   := 0;
+        v_total_comision_empresa := 0;
+        v_total_auditor          := 0;
 
+        -- Comisiones (las que son por auditor completo)
+        v_comision_monto_audit  := calcular_comision_monto_auditado(reg_auditor.id_auditor, :b_fecha);
+        v_comision_prof_critica := calcular_comision_prof_crit(reg_auditor.sueldo, reg_auditor.nivel_criticidad);
+        v_comision_extra        := calcular_comision_extra(reg_auditor.sueldo, reg_auditor.cod_tpcontrato);
 
+        -- Calcular el monto total auditado por el auditor en el mes
+        BEGIN
+            SELECT NVL(SUM(monto_auditoria), 0)
+            INTO v_total_auditor
+            FROM auditoria
+            WHERE id_auditor = reg_auditor.id_auditor
+              AND TO_CHAR(fin_auditoria, 'YYYYMM') = :b_fecha;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                v_total_auditor := 0;
+        END;
 
--- Se inicializan las variables totalizadoras en cero  
-v_res_total_auditores  := 0;
-v_res_total_con_auditorias    := 0;
-v_res_total_sin_auditorias    := 0;
-v_res_monto_total_auditorias  := 0;
-v_res_monto_total_comisiones := 0;
+        -- Si no hay monto total, registrar error
+        IF v_total_auditor = 0 THEN
+            v_correlativo := v_correlativo + 1;
+            INSERT INTO error_proceso (
+                CORRELATIVO, SENTENCIA_ERROR, MENSAJE_ERROR
+            ) VALUES (
+                v_correlativo, 'CALCULO_TOTAL_EMPRESA',
+                'No se pudo calcular Total Empresa para auditor ' || reg_auditor.id_auditor
+            );
+        ELSE
+            -- Bucle por cada empresa auditada por el auditor en el mes
+            FOR reg_emp IN cur_empresas(reg_auditor.id_auditor, :b_fecha) LOOP
+                -- Comisión por cantidad de auditorías (por empresa)
+                v_comision_cant_audit := calcular_comision_cant_auditorias(
+                    reg_auditor.id_auditor,
+                    reg_emp.cod_empresa,
+                    reg_auditor.sueldo,
+                    :b_fecha
+                );
 
------SEGUNDO LOOP
+                -- Total de comisiones por auditor (base para prorrateo)
+                v_total_comision_audit := v_comision_cant_audit
+                                        + v_comision_monto_audit
+                                        + v_comision_prof_critica
+                                        + v_comision_extra;
 
+                -- Aplicar límite
+                IF v_total_comision_audit > :p_valor_limite_com THEN
+                    v_total_comision_audit := :p_valor_limite_com;
+                END IF;
 
-    FOR reg_auditorias IN cur_auditorias(reg_auditor.id_auditor, TO_NUMBER(:p_mes_proceso), TO_NUMBER(:p_anno_proceso)
-    ) LOOP
+                -- Prorrateo por empresa según monto auditado
+                v_total_comision_empresa := v_total_comision_audit
+                                            * (reg_emp.monto_empresa / v_total_auditor);
 
-    
---------CALCULOS
+                -- Insertar en detalle
+                INSERT INTO DETALLE_COMISIONES_AUDITORIAS_MES (
+                    MES_PROCESO,
+                    ANNO_PROCESO,
+                    RUN_AUDITOR,
+                    NOMBRE_AUDITOR,
+                    NOMBRE_PROFESION,
+                    COMISION_TOTAL_AUDIT,
+                    COMISION_MONTO_AUDIT,
+                    COMISION_PROF_CRITICA,
+                    COMISION_EXTRA,
+                    TOTAL_COMISION_AUDIT,
+                    TOTAL_COMISION_EMPRESA,
+                    COD_EMPRESA
+                ) VALUES (
+                    v_mes_proceso,
+                    v_anno_proceso,
+                    reg_auditor.numrun,
+                    reg_auditor.nombre,
+                    reg_auditor.nombre_profesion,
+                    v_comision_cant_audit,
+                    v_comision_monto_audit,
+                    v_comision_prof_critica,
+                    v_comision_extra,
+                    v_total_comision_audit,
+                    v_total_comision_empresa,
+                    reg_emp.cod_empresa
+                );
+            END LOOP; -- reg_emp
+        END IF;
+    END LOOP; -- reg_auditor
 
-----CALCULO POR CANTIDAD DE AUDITORIAS
-v_comision_cant_audit := calcular_comision_cant_auditorias(
-    reg_auditor.id_auditor,
-    reg_auditor.sueldo,
-    :b_fecha
-);
-
-----CALCULO POR MONTO DE AUDITORIAS
-v_comision_monto_audit := calcular_comision_monto_auditado(
-    reg_auditor.id_auditor,
-    :b_fecha
-);
-
-----CALCULO POR PROFESION CRITICA
-
-v_comision_prof_critica:= calcular_comision_prof_crit(
-    reg_auditor.id_auditor,
-    reg_auditor.sueldo,
-    reg_auditor.cod_profesion,
-    :b_fecha
-);
-
------CALCULO POR MONTO EXTRA
-
-v_comision_extra:= calcular_comision_extra(
-    reg_auditor.sueldo,
-    reg_auditor.cod_tpcontrato
-);
-
------TOTAL DE COMISIONES
-
-v_total_comision_audit := v_comision_cant_audit + v_comision_monto_audit + v_comision_prof_critica + v_comision_extra;
-
-
------TOTAL EMPRESA
-
-
-
- -- INSERTAR DATOS EN LA TABLA DE DETALLE
-INSERT INTO detalle_comisiones_auditorias_mes (
-    mes_proceso,
-    anno_proceso,
-    run_auditor,
-    nombre_auditor,
-    nombre_profesion,
-    comision_total_audit,
-    comision_monto_audit,
-    comision_prof_critica,
-    comision_extra,
-    total_comision_audit,
-    total_comision_empresa
-)
-VALUES (
-    SUBSTR(:b_fecha,-2),
-    SUBSTR(:b_fecha,1,4),
-    reg_auditor.numrun || '-' || reg_auditor.dvrun,
-    reg_auditor.nombre || ' ' || reg_auditor.appaterno || ' ' || reg_auditor.apmaterno,
-    (SELECT nombre_profesion FROM profesion WHERE cod_profesion = reg_auditor.cod_profesion),
-    v_comision_cant_audit,
-    v_comision_monto_audit,
-    v_comision_prof_critica,
-    v_comision_extra,
-    v_total_comision_audit,
-    v_total_comision_empresa
-);
-
------CALCULO DE LAS VARIABLES TOTALIZADORAS  
-
-
-END LOOP;
-
- -- INSERTAR DATOS EN LA TABLA DE RESUMEN
-    INSERT INTO resumen_comisiones_auditorias_mes (
-    mes_proceso,
-    anno_proceso,
-    nombre_profesion,
-    total_auditores,
-    total_con_auditorias,
-    total_sin_auditorias,
-    monto_total_auditorias,
-    monto_total_comisiones
-)
-VALUES (
-    SUBSTR(:b_fecha,-2),
-    SUBSTR(:b_fecha,1,4),
-    (SELECT nombre_profesion FROM profesion WHERE cod_profesion = reg_auditor.cod_profesion), 
-    v_res_total_auditores,
-    v_res_total_con_auditorias,
-    v_res_total_sin_auditorias,
-    v_res_monto_total_auditorias,
-    v_res_monto_total_comisiones
-);
-
-END LOOP;
-COMMIT;
-END;  
+    -- Resumen por profesión (usar función con cod_empresa = NULL para total por auditor)
+    INSERT INTO RESUMEN_COMISIONES_AUDITORIAS_MES (
+        MES_PROCESO,
+        ANNO_PROCESO,
+        NOMBRE_PROFESION,
+        TOTAL_AUDITORES,
+        TOTAL_CON_AUDITORIAS,
+        TOTAL_SIN_AUDITORIAS,
+        MONTO_TOTAL_AUDITORIAS,
+        MONTO_TOTAL_COMISIONES
+    )
+    SELECT
+        v_mes_proceso,
+        v_anno_proceso,
+        p.nombre_profesion,
+        COUNT(DISTINCT a.id_auditor) AS total_auditores,
+        COUNT(DISTINCT CASE WHEN au.id_auditor IS NOT NULL THEN a.id_auditor END) AS total_con_auditorias,
+        COUNT(DISTINCT CASE WHEN au.id_auditor IS NULL THEN a.id_auditor END) AS total_sin_auditorias,
+        NVL(SUM(au.monto_auditoria), 0) AS monto_total_auditorias,
+        NVL(SUM(
+            calcular_comision_cant_auditorias(a.id_auditor, NULL, a.sueldo, :b_fecha) +
+            calcular_comision_monto_auditado(a.id_auditor, :b_fecha) +
+            calcular_comision_prof_crit(a.sueldo, p.nivel_criticidad) +
+            calcular_comision_extra(a.sueldo, a.cod_tpcontrato)
+        ), 0) AS monto_total_comisiones
+    FROM
+        auditor a
+        JOIN profesion p ON a.cod_profesion = p.cod_profesion
+        LEFT JOIN auditoria au ON a.id_auditor = au.id_auditor 
+            AND TO_CHAR(au.fin_auditoria, 'YYYYMM') = :b_fecha
+    GROUP BY
+        p.nombre_profesion;
+END;
+/
--- Funciones.sql
+++ Funciones.sql
@@ -1,42 +1,36 @@
---IMPORTANTE: EJECUTAR CADA BLOQUE POR SEPARADO
---Recomiendo que la fecha de proceso en la variable bind encapsule mes y año bajo un formato 'YYYYMM'
-
-DROP FUNCTION calcular_comision_cant_auditorias;
-DROP FUNCTION calcular_comision_monto_auditado;
-DROP FUNCTION calcular_comision_prof_crit;
-DROP FUNCTION calcular_comision_extra;
-DROP FUNCTION calcular_comision_total;
-
---REGLA DE NEGOCIO 1.1 [10-33]
-
-CREATE FUNCTION calcular_comision_cant_auditorias(id_aud IN NUMBER, a_sueldo IN NUMBER, fecha_proceso IN VARCHAR2)
-RETURN NUMBER
-IS
-    v_total_auditorias  NUMBER(5);
-    v_porc_total_audit  NUMBER(4,2);
-    v_comision          NUMBER(10,2);
+CREATE OR REPLACE FUNCTION calcular_comision_cant_auditorias(
+    p_id_aud       IN NUMBER,
+    p_cod_empresa  IN NUMBER,      -- puede venir NULL para total del auditor
+    p_sueldo       IN NUMBER,
+    p_fecha_proc   IN VARCHAR2
+) RETURN NUMBER IS
+    v_total_auditorias  NUMBER(10);
+    v_porc_total_audit  NUMBER(5,2);
+    v_comision          NUMBER(12,2);
 BEGIN
-    SELECT NVL(COUNT(a.monto_auditoria),0) 
+    SELECT COUNT(*)
     INTO v_total_auditorias
-    FROM auditoria a
-    WHERE a.id_auditor = id_aud AND TO_CHAR(a.inicio_auditoria, 'YYYYMM') = fecha_proceso;
-    
-    SELECT pt.porc_total_audit
-    INTO v_porc_total_audit
-    FROM porc_total_auditorias pt
-    WHERE v_total_auditorias BETWEEN pt.total_audit_min AND pt.total_audit_max;
+    FROM auditoria
+    WHERE id_auditor = p_id_aud
+      AND TO_CHAR(fin_auditoria, 'YYYYMM') = p_fecha_proc
+      AND (p_cod_empresa IS NULL OR cod_empresa = p_cod_empresa);
 
-    v_comision := (a_sueldo * v_porc_total_audit) / 100;
-    RETURN ROUND(v_comision,2);
-    
+    SELECT porc_total_audit
+    INTO v_porc_total_audit
+    FROM porc_total_auditorias
+    WHERE v_total_auditorias BETWEEN total_audit_min AND total_audit_max
+      AND ROWNUM = 1;
+
+    v_comision := (p_sueldo * v_porc_total_audit) / 100;
+    RETURN ROUND(v_comision, 2);
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        RETURN 0;  
-END calcular_comision_cant_auditorias;
+        RETURN 0;
+END;
 
 
---REGLA DE NEGOCIO 1.2 [36-59]
-CREATE FUNCTION calcular_comision_monto_auditado(id_aud IN NUMBER, fecha_proceso IN VARCHAR2)
+-- Función para comisión por monto auditado (corregida)
+CREATE OR REPLACE FUNCTION calcular_comision_monto_auditado(id_aud IN NUMBER, fecha_proceso IN VARCHAR2)
 RETURN NUMBER
 IS
     v_total_monto_audit  NUMBER(10);
@@ -46,46 +40,40 @@ BEGIN
     SELECT NVL(SUM(a.monto_auditoria),0) 
     INTO v_total_monto_audit
     FROM auditoria a
-    WHERE a.id_auditor = id_aud AND TO_CHAR(a.inicio_auditoria, 'YYYYMM') = fecha_proceso;
+    WHERE a.id_auditor = id_aud AND TO_CHAR(a.fin_auditoria, 'YYYYMM') = fecha_proceso;
     
-    SELECT pm.porc_monto_audit
+    -- Modificación: Usar ROWNUM para tomar solo una fila en caso de múltiples coincidencias
+    SELECT porc_monto_audit
     INTO v_porc_monto_audit
-    FROM porc_monto_auditorias pm
-    WHERE v_total_monto_audit BETWEEN pm.monto_audit_min AND pm.monto_audit_max;
-    
-    v_comision:=v_total_monto_audit*v_porc_monto_audit;
+    FROM porc_monto_auditorias
+    WHERE v_total_monto_audit BETWEEN monto_audit_min AND monto_audit_max
+      AND ROWNUM = 1; -- Tomamos la primera fila que cumpla la condición
+
+    v_comision := v_total_monto_audit * v_porc_monto_audit;
     RETURN ROUND(v_comision,2);
     
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RETURN 0;
 END calcular_comision_monto_auditado;
+/
 
-
---REGLA DE NEGOCIO 1.3 [62-77]
-CREATE FUNCTION calcular_comision_prof_crit(a_sueldo IN NUMBER, cod_profesion IN NUMBER, fecha_proceso IN VARCHAR2)
-
-RETURN NUMBER
-IS
-    v_comision      NUMBER(10,2);
+-- Función para comisión por profesión crítica (corregida)
+CREATE OR REPLACE FUNCTION calcular_comision_prof_crit(
+    a_sueldo IN NUMBER, 
+    nivel_criticidad IN NUMBER
+) RETURN NUMBER IS
 BEGIN
-    IF(cod_profesion>=3)THEN
-        v_comision:=a_sueldo*0.05;
-
-        RETURN ROUND(v_comision,2);
-
+    IF nivel_criticidad >= 3 THEN
+        RETURN ROUND(a_sueldo * 0.05, 2);
     ELSE
-        v_comision:=0;
-        RETURN v_comision;
-    END IF;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
         RETURN 0;
-END calcular_comision_prof_crit;
-
+    END IF;
+END;
+/
 
 --REGLA DE NEGOCIO 1.4 [74-91]
-CREATE FUNCTION calcular_comision_extra (
+CREATE OR REPLACE FUNCTION calcular_comision_extra (
   a_sueldo IN NUMBER,
   a_tipo_contrato IN NUMBER
 ) RETURN NUMBER IS
@@ -95,7 +83,7 @@ CREATE FUNCTION calcular_comision_extra (
   v_comision NUMBER(10,2);
 BEGIN
 
-  -- Validar que el tipo de contrato esté dentro del rango válido 1-4
+  -- Validar que el tipo de contrato est� dentro del rango v�lido 1-4
   IF a_tipo_contrato BETWEEN 1 AND 4 THEN
     v_comision := a_sueldo * (varray_porc_inc(a_tipo_contrato) / 100);
   ELSE
@@ -108,3 +96,4 @@ EXCEPTION
     RETURN 0;
 END calcular_comision_extra;
 
+/
