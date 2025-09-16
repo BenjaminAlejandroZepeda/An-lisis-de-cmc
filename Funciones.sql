35f7fe92ab9a2734afe00c5b6826496884503bab
GIGOR <148490650+gigor-r@users.noreply.github.com>
2025-09-16 00:10:01 -0300

    Funciones.sql
    Implementación de la regla de negocio 1.5

--- Funciones.sql
+++ Funciones.sql
@@ -1,7 +1,12 @@
 --IMPORTANTE: EJECUTAR CADA BLOQUE POR SEPARADO
 --Recomiendo que la fecha de proceso en la variable bind encapsule mes y año bajo un formato 'YYYYMM'
+DROP FUNCTION calcular_comision_cant_auditorias;
+DROP FUNCTION calcular_comision_monto_auditado;
+DROP FUNCTION calcular_comision_prof_crit;
+DROP FUNCTION calcular_comision_extra;
+DROP FUNCTION calcular_comision_total;
 
---REGLA DE NEGOCIO 1.1 [4-27]
+--REGLA DE NEGOCIO 1.1 [10-33]
 CREATE FUNCTION calcular_comision_cant_auditorias(id_aud IN NUMBER, a_sueldo IN NUMBER, fecha_proceso IN VARCHAR2)
 RETURN NUMBER
 IS
@@ -27,7 +32,7 @@ EXCEPTION
         RETURN 0;  
 END calcular_comision_cant_auditorias;
 
---REGLA DE NEGOCIO 1.2 [30-53]
+--REGLA DE NEGOCIO 1.2 [36-59]
 CREATE FUNCTION calcular_comision_monto_auditado(id_aud IN NUMBER, fecha_proceso IN VARCHAR2)
 RETURN NUMBER
 IS
@@ -53,15 +58,15 @@ EXCEPTION
         RETURN 0;
 END calcular_comision_monto_auditado;
 
---REGLA DE NEGOCIO 1.3 [56-71]
-CREATE FUNCTION calcular_comision_prof_crit(id_aud IN NUMBER, a_sueldo IN NUMBER, cod_profesion IN NUMBER, fecha_proceso IN VARCHAR2)
+--REGLA DE NEGOCIO 1.3 [62-77]
+CREATE FUNCTION calcular_comision_prof_crit(a_sueldo IN NUMBER, cod_profesion IN NUMBER, fecha_proceso IN VARCHAR2)
 RETURN NUMBER
 IS
     v_comision      NUMBER(10,2);
 BEGIN
     IF(cod_profesion>=3)THEN
         v_comision:=a_sueldo*0.05;
-        RETURN v_comision;
+        RETURN ROUND(v_comision,2);
     ELSE
         v_comision:=0;
         RETURN v_comision;
@@ -71,7 +76,7 @@ EXCEPTION
         RETURN 0;
 END calcular_comision_prof_crit;
 
---REGLA DE NEGOCIO 1.4 [74-91]
+--REGLA DE NEGOCIO 1.4 [80-97]
 CREATE FUNCTION calcular_comision_extra(a_sueldo IN NUMBER, a_tipo_contrato IN NUMBER)
 RETURN NUMBER
 IS
@@ -85,8 +90,29 @@ BEGIN
     WHERE a_tipo_contrato=tp.cod_tpcontrato;
     
     v_comision:= (a_sueldo * v_porc_incentivo)/100;
-    RETURN v_comision;
+    RETURN ROUND(v_comision,2);
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RETURN 0;
 END calcular_comision_extra;
+
+--REGLA DE NEGOCIO 1.5 [100-118]
+CREATE FUNCTION calcular_comision_total(id_aud IN NUMBER, a_tipo_contrato IN NUMBER, cod_profesion IN NUMBER, a_sueldo IN NUMBER, fecha_proceso IN VARCHAR2)
+RETURN NUMBER
+IS
+    v_com_cant_aud      NUMBER(10,2);
+    v_com_mon_aud       NUMBER(10,2);
+    v_com_prof_crit     NUMBER(10,2);
+    v_com_extra         NUMBER(10,2);
+    v_comision          NUMBER(10,2);
+BEGIN
+    v_com_cant_aud:=calcular_comision_cant_auditorias(id_aud, a_sueldo, fecha_proceso);
+    v_com_mon_aud:=calcular_comision_monto_auditado(id_aud, fecha_proceso);
+    v_com_prof_crit:=calcular_comision_prof_crit(a_sueldo, cod_profesion, fecha_proceso);
+    v_com_extra:=calcular_comision_extra(a_sueldo, a_tipo_contrato);
+    v_comision:= v_com_cant_aud + v_com_mon_aud + v_com_prof_crit + v_com_extra;
+    RETURN ROUND(v_comision,2);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 0;
+END calcular_comision_total;
